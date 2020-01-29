03d1559fdab3b641a78b882b3fdff908f7f51c8f
gpace1 <33923139+gpace1@users.noreply.github.com>
2020-01-28 23:47:27 -0800

    bo-tie-linux is no longer dependent on bluez

--- base-crates/bo-tie-linux/src/device.rs
+++ base-crates/bo-tie-linux/src/device.rs
@@ -1,4 +1,9 @@
-use std::os::raw::c_void;
+//! Linux Bluetooth device functionality controller methods
+//!
+//! These are functions that are used to control the interface to the bluetooth devices on the
+//! system. This isn't a complete implemenation of control commands/operations, it is just the
+//! functionality used by this libary. These are linux specific and have no relation to the
+//! bluetooth specification.
 
 pub const BTPROTO_HCI: i32 = 1;
 
@@ -8,11 +13,11 @@ const BLU_DEV_LIST_DEFAULT_CNT: usize = 16;
 // pub const HCI_CHANNEL_RAW: i32 = 0; // A raw channel works with the linux hci implementation
 pub const HCI_CHANNEL_USER: i32 = 1; // User channel gives total control, but requires hci
 
-#[link(name = "bluetooth")]
-extern "C" {
-    // pub fn hci_get_route(bt_dev_addr: *mut bo_tie::BluetoothDeviceAddress) -> i32;
-    // pub fn hci_send_cmd(dev: i32, ogf: u16, ocf: u16, parameter_len: u8, parameter: *mut c_void) -> i32;
-}
+// #[link(name = "bluetooth")]
+// extern "C" {
+//     pub fn hci_get_route(bt_dev_addr: *mut bo_tie::BluetoothDeviceAddress) -> i32;
+//     pub fn hci_send_cmd(dev: i32, ogf: u16, ocf: u16, parameter_len: u8, parameter: *mut c_void) -> i32;
+// }
 
 pub mod hci {
 
@@ -28,8 +33,9 @@ pub mod hci {
     /// address* (the address either hard coded or burned onto the controller). If any bluetooth
     /// controller will do, then `None` can be provided to get a controller.
     ///
-    /// The return is the hci device number along of the first non-*raw* device that is found. This
-    /// library uses `HCI_CHANNEL_USER` which requires the device to be non-raw.
+    /// This will match the first device that does not contain the `HCI_RAW` flag. This flag
+    /// indicates that the interface is unconfigured and an unconfigured device cannot be bound
+    /// to with `HCI_CHANNEL_USER`
     ///
     /// # Note
     /// This function will scan a maximum 16 devices. Looking at the kernel the limit seems more
@@ -37,52 +43,50 @@ pub mod hci {
     /// hci_get_dev_list in /net/bluetooth/hci_core.c. When `const_generics` is stable, this
     /// function should be implemented to accept a const generic for the device count.
     pub fn get_dev_id<A>(device_address: A ) -> Result<usize, nix::Error>
-        where A: Into<Option<bo_te::BluetoothDeviceAddress>>
+        where A: Into<Option<bo_tie::BluetoothDeviceAddress>>
     {
         use nix::libc;
+        use std::os::raw::c_int;
 
         let mut hci_dev_list = super::hci_dev_list_req::default();
 
         let sock = unsafe {
-            let raw_fd = libc::socket( libc::AF_BLUETOOTH, libc::SOCK_RAW | libc::SOCK_CLOEXEC, super::BTPROTO_HCI);
+            let raw_fd = libc::socket(libc::AF_BLUETOOTH, libc::SOCK_RAW | libc::SOCK_CLOEXEC, super::BTPROTO_HCI);
 
             if raw_fd < 0 {
-                return Err( nix::errno::errno().into() )
+                return Err( nix::errno::Errno::last().into() )
             }
 
-            crate::ArcFileDesc::from(raw_fd)
+            crate::FileDescriptor(raw_fd)
         };
 
-        unsafe {
-            if super::hci_get_dev_list(sock, &mut hci_dev_list as *mut _) < 0 {
-                return Err( nix::errno::errno().into() )
-            }
-        };
+        unsafe { super::hci_get_dev_list(sock.0, &mut hci_dev_list as *mut _ as *mut c_int)? };
 
         let device_address_opt = device_address.into();
 
-        for dev_req in hci_dev_list.dev_req[..hci_dev_list.dev_num as usize] {
+        for dev_req in &hci_dev_list.dev_req[..hci_dev_list.dev_num as usize] {
             let mut dev_info = super::hci_dev_info::default();
 
             dev_info.dev_id = dev_req.dev_id;
 
-            if unsafe { super::hci_get_dev_info(sock, &mut dev_info as *mut _) } != 0 ||
-               ! test_flag(HCI_RAW, &[dev_info.flags]) {
+            let di_rslt = unsafe {
+                super::hci_get_dev_info(sock.0, &mut dev_info as *mut _ as *mut c_int)
+            };
+
+            if di_rslt.is_err() || test_flag(HCI_RAW, &[dev_info.flags]) {
                 continue
             }
 
             match device_address_opt {
-                None => return Ok(dev_req.dev_id),
-                Some(addr) => if addr == dev_info.bdaddr { return Ok(dev_req.dev_id) },
+                None => return Ok(dev_req.dev_id as usize),
+                Some(addr) => if addr == dev_info.bdaddr { return Ok(dev_req.dev_id as usize) },
             }
         }
 
-        Err( nix::errno::Errno::EEXIST.into() )
+        Err( nix::errno::Errno::ENODEV.into() )
     }
 
     /// Send a command to the bluetooth controller
-    ///
-    /// The implementation of `send_command` is based of the `hci_send_cmd` function in bluez
     pub fn send_command<P>(dev: &crate::FileDescriptor, parameter: &P) -> nix::Result<usize>
         where P: bo_tie::hci::CommandParameter
     {
@@ -144,12 +148,12 @@ impl Default for hci_dev_list_req {
     }
 }
 
-#[repr(c)]
+#[repr(C)]
 #[derive(Default)]
-struct hci_dev_info {
+pub struct hci_dev_info {
     dev_id: u16,
     name: [std::os::raw::c_char;8],
-    bdaddr: crate::BluetoothDeviceAddress,
+    bdaddr: bo_tie::BluetoothDeviceAddress,
     flags: u32,
     r#type: u8,
     features: [u8;8],
@@ -163,9 +167,9 @@ struct hci_dev_info {
     stat: hci_dev_stats
 }
 
-#[repr(c)]
+#[repr(C)]
 #[derive(Default)]
-struct hci_dev_stats {
+pub struct hci_dev_stats {
     err_rx: u32,
     err_tx: u32,
     cmd_tx: u32,
@@ -187,5 +191,5 @@ const HCI_IOC_HCIGETDEVINFO: u8 = 211;
 
 nix::ioctl_write_int!(hci_dev_up, HCI_IOC_MAGIC, HCI_IOC_HCIDEVUP);
 nix::ioctl_write_int!(hci_dev_down, HCI_IOC_MAGIC, HCI_IOC_HCIDEVDOWN);
-nix::ioctl_read!(hci_get_dev_list, HCI_IOC_MAGIC, HCI_IOC_HCIGETDEVLIST, hci_dev_list_req);
-nix::ioctl_read!(hci_get_dev_info, HCI_IOC_MAGIC, HCI_IOC_HCIGETDEVINFO, hci_dev_info);
+nix::ioctl_read!(hci_get_dev_list, HCI_IOC_MAGIC, HCI_IOC_HCIGETDEVLIST, std::os::raw::c_int);
+nix::ioctl_read!(hci_get_dev_info, HCI_IOC_MAGIC, HCI_IOC_HCIGETDEVINFO, std::os::raw::c_int);
