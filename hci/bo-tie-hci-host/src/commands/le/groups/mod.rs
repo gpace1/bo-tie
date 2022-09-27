b52a7783a62900d5aad42b2c86a853ad69ae0d8e
gpace1 <33923139+gpace1@users.noreply.github.com>
2022-09-27 14:05:50 -0700

    removing feature guards
    these features were removed before but the guards were left by accident

--- hci/bo-tie-hci-host/src/commands/le/groups/mod.rs
+++ hci/bo-tie-hci-host/src/commands/le/groups/mod.rs
@@ -4,16 +4,10 @@
 //! controller. The organization of commands in modules is based on the *LE Controller Requirements*
 //! in the Bluetooth Core Specification found at Vol 4, Part E, section 3.
 
-#[cfg(feature = "le-connection-parameters")]
 pub mod con_pram_req;
-#[cfg(feature = "le-connection")]
 pub mod connection;
-#[cfg(feature = "le-encryption")]
 pub mod encryption;
 pub mod mandatory;
-#[cfg(feature = "le-privacy")]
 pub mod privacy;
-#[cfg(feature = "le-receiver")]
 pub mod receiver;
-#[cfg(feature = "le-transmitter")]
 pub mod transmitter;
