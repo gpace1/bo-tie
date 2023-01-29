c91458ca4a4dafd32ec004637fdc90139ead6983
gpace1 <33923139+gpace1@users.noreply.github.com>
2023-01-29 09:02:20 -0800

    feature gating modules

--- bo-tie/src/host.rs
+++ bo-tie/src/host.rs
@@ -1,13 +1,22 @@
 //! The host protocols
 
+#[cfg(feature = "att")]
 #[doc(inline)]
 pub use bo_tie_att as att;
+
 #[doc(inline)]
+#[cfg(feature = "gap")]
 pub use bo_tie_gap as gap;
+
+#[cfg(feature = "gatt")]
 #[doc(inline)]
 pub use bo_tie_gatt as gatt;
+
+#[cfg(feature = "l2cap")]
 #[doc(inline)]
 pub use bo_tie_l2cap as l2cap;
+
+#[cfg(feature = "sm")]
 #[doc(inline)]
 pub use bo_tie_sm as sm;
 
