99e74e3a76d86147273ac99ad791580bf2bc0f87
gpace1 <33923139+gpace1@users.noreply.github.com>
2023-09-03 14:59:54 -0700

    moving Rendezvous type to test crate

--- host/tests/src/lib.rs
+++ host/tests/src/lib.rs
@@ -35,14 +35,14 @@ pub fn create_le_link(max_tx_size: usize) -> (LeULogicalLink<PhysicalLink>, LeUL
 /// link. Calling the send closure will induce a reception of that fragment by the physical link and
 /// calling the recv closure will receive a fragment sent by the physical link.
 ///
-/// The input `max_tx_size` is the maximum transmission size of the created physical link, however 
+/// The input `max_tx_size` is the maximum transmission size of the created physical link, however
 /// it is not the maximum size for the sender closure (there is no max size for this closure).
-/// 
-/// If the receiver is called and there is no data in channel to the logical link, then `None` is 
+///
+/// If the receiver is called and there is no data in channel to the logical link, then `None` is
 /// returned by the closure.
-/// 
+///
 /// # Panics
-/// There is no panic calling this method, but the returned sender and receiver will panic if 
+/// There is no panic calling this method, but the returned sender and receiver will panic if
 /// called and the logical link has been dropped.
 pub fn create_le_false_link(
     max_tx_size: usize,
@@ -54,4 +54,41 @@ pub fn create_le_false_link(
     let (phy_link_a, sender, receiver) = PhysicalLink::new_false_connection(max_tx_size);
 
     (LeULogicalLink::new(phy_link_a), sender, receiver)
+}
+
+/// A simple rendezvous implementation
+///
+/// Unlike a barrier, a `Rendezvous` only works between two tasks and only clears when every task
+/// is awaiting the `Rendezvous` at the same time (a barrier increases its clear count the moment
+/// it is polled, which can cause issues with something like the `select!` macro).
+pub struct Rendezvous {
+    sender: tokio::sync::oneshot::Sender<()>,
+    receiver: tokio::sync::oneshot::Receiver<()>,
+}
+
+impl Rendezvous {
+    /// Rendezvous with the other task
+    ///
+    /// The return is the output value by the other `Rendezvous`'s task.
+    pub async fn rendez(self) {
+        self.sender.send(()).ok();
+
+        self.receiver.await.expect("other Rendezvous dropped")
+    }
+}
+
+pub fn rendezvous() -> (Rendezvous, Rendezvous) {
+    let (sender_1, receiver_1) = tokio::sync::oneshot::channel();
+    let (sender_2, receiver_2) = tokio::sync::oneshot::channel();
+
+    (
+        Rendezvous {
+            sender: sender_1,
+            receiver: receiver_2,
+        },
+        Rendezvous {
+            sender: sender_2,
+            receiver: receiver_1,
+        },
+    )
 }
