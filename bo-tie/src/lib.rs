f60aaeadb68b55c863ec69f3a959a26bbe2fdd38
gpace1 <gregpace@night-mesh.com>
2024-10-18 12:29:12 -0700

    adding reexport for trait TryExtend

--- bo-tie/src/lib.rs
+++ bo-tie/src/lib.rs
@@ -1,7 +1,7 @@
 //! A Bluetooth Library
 //!
 //! The primary purpose of bo-tie is to provide a middling layer between the architecture specific
-//! and user friendly libraries for Bluetooth. bo-tie is also intended to be used in environments
+//! and user-friendly libraries for Bluetooth. bo-tie is also intended to be used in environments
 //! where only [`core`](https://doc.rust-lang.org/core/) and
 //! [`alloc`](https://doc.rust-lang.org/alloc/) are available.
 //!
@@ -9,7 +9,6 @@
 //! Interface). All commands, events, *and* data (ACL, SCO/eSCO) go through this interface.
 
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![cfg_attr(test, feature(test))]
 #![cfg_attr(not(test), no_std)]
 
 #[cfg(feature = "hci")]
@@ -25,3 +24,4 @@ pub mod host;
 
 pub use bo_tie_core::errors::Error;
 pub use bo_tie_core::{BluetoothDeviceAddress, DeviceFeatures, Features, LeDeviceFeatures, LeFeatures};
+pub use bo_tie_core::buffer::TryExtend;
